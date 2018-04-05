61050342ecdfad7332f84c62119706c2cf0b5ec3
Yura Kolodiy <timber2017@gmail.com>
2018-04-05 23:46:16 +0300

    add skills

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const progresses = document.querySelectorAll('.skills__line');
+const progress = document.querySelector('.skills');
+
+function isVisible(elem) {
+    let coords = elem.getBoundingClientRect();
+    let windowHeight = document.documentElement.clientHeight;
+    // верхняя граница elem в пределах видимости ИЛИ нижняя граница видима
+    let topVisible = coords.top > 0 && coords.top < windowHeight;
+    let bottomVisible = coords.bottom < windowHeight && coords.bottom > 0;
+    return topVisible || bottomVisible;
+}
+function showProgress() {
+    if(isVisible(progress)){
+        setTimeout(function () {
+            [...progresses].map(function (item) {
+                item.style.width = item.parentNode.previousElementSibling.querySelector('span').textContent;
+            })
+        }, 500);
+        window.removeEventListener('scroll', showProgress);
+    }
+}
+
+window.addEventListener('scroll', showProgress);
+showProgress();
